daf9c249d7184bbe4509c4e432ed7e1c82f3f19d
syedhaideralizaidi <haider.zaidiy@gmail.com>
2024-03-26 12:21:16 +0500

    file added changes

--- utils/storages.py
+++ utils/storages.py
@@ -4,17 +4,21 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticRootS3Boto3Storage(S3Boto3Storage):
     location = settings.AWS_STATIC_LOCATION
+    default_acl = "public-read"
     # default_acl = None  # Remove the ACL setting
 
 
 class MediaRootS3Boto3Storage(S3Boto3Storage):
     location = settings.AWS_PUBLIC_MEDIA_LOCATION
     file_overwrite = False
-    default_acl = None  # Remove the ACL setting
+    default_acl = "public-read"
+
+    # default_acl = None  # Remove the ACL setting
 
 
 class PrivateMediaStorage(S3Boto3Storage):
     location = settings.AWS_PRIVATE_MEDIA_LOCATION
-    default_acl = None  # Remove the ACL setting
+    # default_acl = None  # Remove the ACL setting
     file_overwrite = False
     custom_domain = False
+    default_acl = "private"
