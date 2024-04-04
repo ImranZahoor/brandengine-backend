692a3d14cf06fcfb0ef8084b3771f87608563e41
syedhaideralizaidi <haider.zaidiy@gmail.com>
2024-04-05 00:37:48 +0500

    deploy check

--- brandprofile/views.py
+++ brandprofile/views.py
@@ -1,10 +1,8 @@
 import json
-
 from rest_framework import viewsets, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
 from brandprofile.forms import FileUploadForm
 from brandprofile.models import BrandProfile, Category
 from brandprofile.serializers import BrandProfileSerializer, CategorySerializer
