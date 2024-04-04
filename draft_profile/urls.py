2471625f81866f5d35b787707f151171869ac648
syedhaideralizaidi <haider.zaidiy@gmail.com>
2024-04-04 12:29:58 +0500

    api issue fix

--- draft_profile/urls.py
+++ draft_profile/urls.py
@@ -4,16 +4,16 @@ from rest_framework.routers import DefaultRouter
 from draft_profile.views import DraftProfileViewSet, UploadCSVView, MigrateBrands
 
 app_name = "draft"
-router = DefaultRouter()
+router = DefaultRouter(trailing_slash=True)
 router.register("draft_profile", DraftProfileViewSet, basename="draft_profile")
 
 urlpatterns = [
+    path(
+        "draft_profile/migrate",
+        DraftProfileViewSet.as_view({"post": "migrate_brands"}),
+        name="migrate_draft_profile",
+    ),
     path("", include(router.urls)),
     path("draft_profile/upload", UploadCSVView.as_view(), name="upload_csv_draft"),
-    # path(
-    #     "draft_profile/migrate",
-    #     DraftProfileViewSet.as_view({"post": "migrate_brands"}),
-    #     name="migrate_draft_profile",
-    # ),
-    path("draft_profile/migrate", MigrateBrands.as_view(), name="migrate"),
+    # path("draft_profile/migrate", MigrateBrands.as_view(), name="migrate"),
 ]
